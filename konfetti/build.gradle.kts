3c9cc5d3f0ca2c1a373b117190ef29c743b58747
Vincent Guillebaud <vincent.guillebaud31@gmail.com>
2025-01-17 20:29:27 +0100

    🎉 First commit of Konfetti

---
+++ konfetti/build.gradle.kts
@@ -0,0 +1,58 @@
+import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.composeMultiplatform)
+    alias(libs.plugins.composeCompiler)
+    alias(libs.plugins.vanniktech.mavenPublish)
+}
+
+kotlin {
+    explicitApi()
+
+    androidTarget {
+        publishLibraryVariants("release")
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_11)
+        }
+    }
+    
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach { iosTarget ->
+        iosTarget.binaries.framework {
+            baseName = "ComposeApp"
+            isStatic = true
+        }
+    }
+    
+    jvm()
+    
+    @OptIn(ExperimentalWasmDsl::class)
+    listOf (
+        js(),
+        wasmJs(),
+    ).forEach {
+        it.moduleName = "Konfetti"
+        it.browser()
+    }
+    
+    sourceSets {
+        commonMain.dependencies {
+            // Compose
+            implementation(compose.runtime)
+        }
+    }
+}
+
+android {
+    namespace = "io.github.vinceglb.konfetti"
+    compileSdk = libs.versions.android.compileSdk.get().toInt()
+    defaultConfig {
+        minSdk = libs.versions.android.minSdk.get().toInt()
+    }
+}
