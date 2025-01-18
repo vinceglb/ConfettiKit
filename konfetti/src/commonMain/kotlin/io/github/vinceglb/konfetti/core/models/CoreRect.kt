59fc2ce02b7a89476b0f06ccd28b615fa20b82d2
Vincent Guillebaud <vincent.guillebaud31@gmail.com>
2025-01-18 15:17:13 +0100

    👽️ Add Konfetti Core models

---
+++ konfetti/src/commonMain/kotlin/io/github/vinceglb/konfetti/core/models/CoreRect.kt
@@ -0,0 +1,22 @@
+package io.github.vinceglb.konfetti.core.models
+
+internal sealed interface CoreRect {
+    val x: Float
+    val y: Float
+    val width: Float
+    val height: Float
+
+    fun contains(
+        px: Int,
+        py: Int,
+    ): Boolean {
+        return px >= x && px <= x + width && py >= y && py <= y + height
+    }
+
+    data class CoreRectImpl(
+        override val x: Float = 0f,
+        override val y: Float = 0f,
+        override val width: Float = 0f,
+        override val height: Float = 0f,
+    ) : CoreRect
+}
