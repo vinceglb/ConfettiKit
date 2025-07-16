67544c73727ff8a94c3ce9656619470e742de36c
HansHolz09 <Istdaniemand8@gmail.com>
2025-07-16 18:41:56 +0200

    Added CustomShape, Image and Vector to Shapes + Updated Sample

--- confettikit/src/commonMain/kotlin/io/github/vinceglb/confettikit/compose/DrawShapes.kt
+++ confettikit/src/commonMain/kotlin/io/github/vinceglb/confettikit/compose/DrawShapes.kt
@@ -3,7 +3,13 @@ package io.github.vinceglb.confettikit.compose
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.Outline
+import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.drawscope.DrawScope
+import androidx.compose.ui.graphics.drawscope.translate
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.IntSize
 import io.github.vinceglb.confettikit.core.Particle
 import io.github.vinceglb.confettikit.core.models.Shape
 
@@ -40,6 +46,54 @@ internal fun Shape.draw(
                 topLeft = Offset(particle.x, particle.y),
                 size = Size(size, height),
             )
+        }
+        is Shape.CustomShape -> {
+            val outline = shape.createOutline(
+                size = Size(particle.width, particle.height),
+                layoutDirection = drawScope.layoutDirection,
+                density = drawScope,
+            )
+            drawScope.translate(particle.x, particle.y) {
+                when (outline) {
+                    is Outline.Generic -> {
+                        drawScope.drawPath(
+                            path = outline.path,
+                            color = Color(particle.color),
+                        )
+                    }
+                    is Outline.Rectangle -> {
+                        drawScope.drawRect(
+                            topLeft = outline.rect.topLeft,
+                            size = outline.rect.size,
+                            color = Color(particle.color),
+                        )
+                    }
+                    is Outline.Rounded -> {
+                        val path = Path().apply { addRoundRect(outline.roundRect) }
+                        drawScope.drawPath(
+                            path = path,
+                            color = Color(particle.color),
+                        )
+                    }
+                }
+            }
+        }
+        is Shape.Image -> {
+            drawScope.drawImage(
+                image = imageBitmap,
+                dstSize = IntSize(particle.width.toInt(), particle.height.toInt()),
+                dstOffset = IntOffset(particle.x.toInt(), particle.y.toInt()),
+            )
+        }
+        is Shape.Vector -> {
+            with(vectorPainter) {
+                drawScope.translate(particle.x, particle.y) {
+                    draw(
+                        size = Size(particle.width, particle.height),
+                        colorFilter = ColorFilter.tint(Color(particle.color)),
+                    )
+                }
+            }
         }
     }
 }
--- sample/composeApp/src/commonMain/kotlin/io/github/vinceglb/confettikit/sample/Presets.kt
+++ sample/composeApp/src/commonMain/kotlin/io/github/vinceglb/confettikit/sample/Presets.kt
@@ -1,10 +1,21 @@
 package io.github.vinceglb.confettikit.sample
 
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Celebration
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
+import androidx.compose.ui.unit.dp
+import confettikit_lib.sample.composeapp.generated.resources.Res
+import confettikit_lib.sample.composeapp.generated.resources.confetti
 import io.github.vinceglb.confettikit.core.Angle
 import io.github.vinceglb.confettikit.core.Party
 import io.github.vinceglb.confettikit.core.Position
 import io.github.vinceglb.confettikit.core.Spread
 import io.github.vinceglb.confettikit.core.emitter.Emitter
+import io.github.vinceglb.confettikit.core.models.Shape
+import org.jetbrains.compose.resources.imageResource
 import kotlin.time.Duration.Companion.milliseconds
 import kotlin.time.Duration.Companion.seconds
 
@@ -18,6 +29,11 @@ class Presets {
                     damping = 0.9f,
                     spread = 360,
                     colors = listOf(0xfce18a, 0xff726d, 0xf4306d, 0xb48def),
+                    shapes = listOf(
+                        Shape.CustomShape(CircleShape),
+                        Shape.CustomShape(HeartShape()),
+                        Shape.CustomShape(RoundedCornerShape(5.dp))
+                    ),
                     emitter = Emitter(duration = 100.milliseconds).max(100),
                     // position = Position.Relative(0.5, 0.3)
                 )
@@ -45,7 +61,10 @@ class Presets {
             )
         }
 
+        @Composable
         fun rain(): List<Party> {
+            val imageBitmap = imageResource(Res.drawable.confetti)
+            val vectorPainter = rememberVectorPainter(Icons.Outlined.Celebration)
             return listOf(
                 Party(
                     speed = 0f,
@@ -54,13 +73,15 @@ class Presets {
                     angle = Angle.BOTTOM,
                     spread = Spread.ROUND,
                     colors = listOf(0xfce18a, 0xff726d, 0xf4306d, 0xb48def),
+                    shapes = listOf(Shape.Image(imageBitmap), Shape.Vector(vectorPainter)),
                     emitter = Emitter(duration = 5.seconds).perSecond(100),
                     position = Position.Relative(0.0, 0.0).between(Position.Relative(1.0, 0.0))
                 )
             )
         }
 
-        val all = listOf(
+        @Composable
+        fun all() = listOf(
             explode(),
             parade(),
             rain(),
